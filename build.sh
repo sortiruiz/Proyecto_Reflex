c7e5c02143b07714e91a4e581e62501a63290379
Samuel Ortiz <sortiz@domain-system.local>
2024-07-03 01:46:06 +0200

    Nueva API WEB en RAILWAY añadida

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-venv/Scripts/activate
+#venv/Scripts/activate
 #pip install -r requirements.txt
-#reflex init 
-reflex export --frontend-only #me exporta la parte estatica 
+reflex init 
+API_URL=https://sortiruiz-web.up.railway.app reflex export --frontend-only #me exporta la parte estatica 
+
 #desde terminal ubuntu 
 wsl rm -rf public #esto me borra la antigua carpeta public
 wsl unzip  frontend.zip -d public #esto me lo unzipea a public 
